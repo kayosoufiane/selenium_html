0289fa9ce9470f736a382f4513091864a29cc393
skhattab <soufiane.khattab@gmail.com>
2015-02-07 23:52:57 +0100

    first commit

---
+++ selenium_html/src/main/java/nl/berg/packt/selenium/App.java
@@ -0,0 +1,13 @@
+package nl.berg.packt.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
